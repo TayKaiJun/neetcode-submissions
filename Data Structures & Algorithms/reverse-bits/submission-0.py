45418bed94c967d449f3d2fb19c0c110fe305370
Kai Jun Tay <tehohno@gmail.com>
2026-06-05 17:51:42 -0700

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        r = 0
+        for _ in range(32):
+            r = r << 1
+            r += n & 1
+            n = n >> 1
+        
+        return r
+            
